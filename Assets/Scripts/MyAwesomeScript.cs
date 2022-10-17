b54c41baf7eb875e0ae09a7e1473b66a45d12e2c
EagleFG <wesley.n.campbell@gmail.com>
2022-10-17 00:33:59 -0700

    Modified scene and added new script

---
+++ Assets/Scripts/MyAwesomeScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MyAwesomeScript : MonoBehaviour
+{
+    private bool isAwesome;
+
+    void Start()
+    {
+        MakeAwesome();
+    }
+
+    private void MakeAwesome()
+    {
+        isAwesome = true;
+    }
+}
